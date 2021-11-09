cb037141ebd4b4e0a16bdb264d9f4bbf0d0364fe
chernihou <cherni.houneida@gmail.com>
2021-11-09 14:53:50 +0100

    "modification des models"

--- sporty/src/main/java/com/spring/sporty/model/Categorie.java
+++ sporty/src/main/java/com/spring/sporty/model/Categorie.java
@@ -1,12 +1,12 @@
 package com.spring.sporty.model;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.List;
-
 @Getter
 @Setter
 //@NoArgsConstructor
@@ -16,41 +16,20 @@ import java.util.List;
 public class Categorie {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id_cat;
+    private String id;
     private String nom_cat;
 
     @OneToMany(mappedBy = "categorie",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
     private List<Produit> Produits;
 
-    public String getId_cat() {
-        return id_cat;
-    }
 
-    public void setId_cat(String id_cat) {
-        this.id_cat = id_cat;
-    }
 
-    public String getNom_cat() {
-        return nom_cat;
-    }
-
-    public void setNom_cat(String nom_cat) {
-        this.nom_cat = nom_cat;
-    }
-
-    public List<Produit> getProduits() {
-        return Produits;
-    }
-
-    public void setProduits(List<Produit> produits) {
-        Produits = produits;
-    }
 
     public Categorie() {
     }
 
     public Categorie(String id_cat, String nom_cat) {
-        this.id_cat = id_cat;
+        this.id = id_cat;
         this.nom_cat = nom_cat;
     }
 }
--- sporty/src/main/java/com/spring/sporty/model/Produit.java
+++ sporty/src/main/java/com/spring/sporty/model/Produit.java
@@ -8,8 +8,6 @@ import java.util.List;
 
 @Getter
 @Setter
-
-
 @Entity
 @Table(name="produits")
 public class Produit {
@@ -25,54 +23,6 @@ public class Produit {
     @ManyToOne(optional = false)
     @JoinColumn(name="marque_id", referencedColumnName = "id")
     private Marque marque;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDesignation() {
-        return designation;
-    }
-
-    public void setDesignation(String designation) {
-        this.designation = designation;
-    }
-
-    public double getPrix() {
-        return prix;
-    }
-
-    public void setPrix(double prix) {
-        this.prix = prix;
-    }
-
-    public int getQuantite() {
-        return quantite;
-    }
-
-    public void setQuantite(int quantite) {
-        this.quantite = quantite;
-    }
-
-    public Categorie getCategorie() {
-        return categorie;
-    }
-
-    public void setCategorie(Categorie categorie) {
-        this.categorie = categorie;
-    }
-
-    public Marque getMarque() {
-        return marque;
-    }
-
-    public void setMarque(Marque marque) {
-        this.marque = marque;
-    }
 
     public Produit() {
     }
