5cadcb0bf76785b4f4d8b114daa693b38191bfad
wassim khazri <wassimkhazri@gmail.com>
2021-11-09 14:56:51 +0100

    "modi"

--- sporty/src/main/java/com/spring/sporty/model/Marque.java
+++ sporty/src/main/java/com/spring/sporty/model/Marque.java
@@ -16,40 +16,19 @@ import java.util.List;
 public class Marque {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private String id_marque;
+    private String id;
     private String nom_marque;
     @OneToMany(mappedBy = "marque",cascade=CascadeType.ALL,fetch=FetchType.LAZY)
     private List<Produit> Produits;
 
-    public String getId_marque() {
-        return id_marque;
-    }
 
-    public void setId_marque(String id_marque) {
-        this.id_marque = id_marque;
-    }
 
-    public String getNom_marque() {
-        return nom_marque;
-    }
-
-    public void setNom_marque(String nom_marque) {
-        this.nom_marque = nom_marque;
-    }
-
-    public List<Produit> getProduits() {
-        return Produits;
-    }
-
-    public void setProduits(List<Produit> produits) {
-        Produits = produits;
-    }
 
     public Marque() {
     }
 
-    public Marque(String id_marque, String nom_marque) {
-        this.id_marque = id_marque;
+    public Marque(String id, String nom_marque) {
+        this.id = id;
         this.nom_marque = nom_marque;
     }
 }
